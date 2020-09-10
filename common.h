678aeac80a3a2df023b6295705c485805a309cdd
mosad <abdelrhman.m0s3d@gmail.com>
2020-09-10 20:39:14 +0200

    data added

---
+++ common.h
@@ -0,0 +1,10 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+typedef struct{
+    char *name;
+    char *year;
+    int id;
+    char *password;
+}data_t;
+
+#endif
--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "common.h"
+data_t student[100] = {
+{"Mohmed Ahmed" , "2nd", 129789 , "12345"},
+{"Mohmed ali" , "2nd", 129788 , "12345"},
+{"Mohmed samy" , "2nd", 129787 , "12345"}
+};
+char index = 3 ;
 int main()
 {
 
