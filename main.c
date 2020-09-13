6c0c89814cb5b6c97d170d8b9af64d34b1241395
mosad <abdelrhman.m0s3d@gmail.com>
2020-09-13 08:45:10 +0200

    nothing

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "common.h"
+#include "admin.h"
+
 data_t student[100] = {
 {"Mohmed Ahmed" , "2nd", 129789 , "12345"},
 {"Mohmed ali" , "2nd", 129788 , "12345"},
@@ -9,6 +11,6 @@ data_t student[100] = {
 char index = 3 ;
 int main()
 {
-
+   add(3);
     return 0;
 }
