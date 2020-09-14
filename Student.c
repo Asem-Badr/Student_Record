1171156cd833bd9eb7c80cda9d989bf4da94e3b0
AbdelrahmanElshahat <70945443+AbdelrahmanElshahat@users.noreply.github.com>
2020-09-14 04:04:56 +0200

    last edit ISA

--- Student.c
+++ Student.c
@@ -2,49 +2,65 @@
 #include <string.h>
 #include "student.h"
 #include "common.h"
+int i;
 int j;
+
 extern data_t student[100];
 
-void student_login(){
-    int i;
+void student_login()
+{
+
     char username[50];
     char userpass[10];
-    again:printf("Enter UserName :");
+    printf("Enter UserName :");
     scanf("%s",username);
     printf("Enter Password :");
     scanf("%s",userpass);
-   for(i=0;i<100;i++){
-    if(strcmp(student[i].name,username)==0 && strcmp(student[i].password,userpass)==0){
-        student_screen();
+    for(i=0; i<100; i++)
+    {
+
         i=j;
-    }else {
+        student_screen();
+        break;
+
+    }
+    while(!(strcmp(student[i].name,username)==0 && strcmp(student[i].password,userpass)==0))
+    {
         printf("Invalid username or password\n ");
-        goto again;
+        printf("Enter UserName :");
+        scanf("%s",username);
+        printf("Enter Password :");
+        scanf("%s",userpass);
     }
 
-   }
+
 }
-void student_screen(){
-    char choice;
-    loop:printf("\t\t\t\t a.View your record.\t\t\t\t\n\t\t\t\t b.Edit your password.\t\t\t\t\n");
-    scanf("%c",&choice);
-    switch(choice){
-        case 'a' : view_record();
-        break;
-        case 'b' : edit_password();
-        break;
-        default: printf("Wrong entry \n");
-        goto loop;
-        break;
+
+void student_screen()
+{
+    int choice;
+    printf("\t\t\t\t 1.View your record.\t\t\t\t\n\t\t\t\t 2.Edit your password.\t\t\t\t\n");
+    scanf("%d",&choice);
+    if(choice==1)
+    {
+        view_record();
     }
+    else if(choice==2)
+    {
+        edit_password();
+    }
+
 }
-void view_record(){
+void view_record()
+{
     printf("Name : %s\n Year : %s\n ID : %d\n",student[j].name,student[j].year,student[j].id);
 
 }
-void edit_password(){
-    char newpassword[10];
+void edit_password()
+{
+    char buffer[100];
     printf("Enter new password : ");
-    scanf("%s",newpassword);
-    strcpy(student[j].password,newpassword);
+    scanf("%s",&buffer);
+    student[j].password=(char*)malloc(strlen(buffer)+1);
+    strcpy(student[j].password,buffer);
 }
