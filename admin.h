49648a4f13dfbaee9887ee33978dd9e78b19888b
Asem-Badr <70919962+Asem-Badr@users.noreply.github.com>
2020-09-15 00:51:32 +0200

    last edit ISA

--- admin.h
+++ admin.h
@@ -1,11 +1,13 @@
 #ifndef ADMIN_H
 #define ADMIN_H
-void add (int arr_index);
-void rmv (int arr_index);
-void edit (int arr_index);
-void view (int arr_index);
+//void init(void);
+void add (void);
+void rmv (void);
+void edit (void);
+void view (int x);//don't use this in main
+void view_student(void);//use this one
 void view_all(void);
 void edit_admin (void);
-
-
+void admin_login(void);
+void admin_screen(void);
 #endif // ADMIN_H_INCLUDED
